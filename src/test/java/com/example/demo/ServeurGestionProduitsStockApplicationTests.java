461b6f8158297e8b5985e692301a6020e16ff387
Saadoune <saad.meryem95@gmail.com>
2020-02-22 21:32:29 +0100

    first commit

---
+++ src/test/java/com/example/demo/ServeurGestionProduitsStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionProduitsStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
